f11a26e3468c67901152122665abca7eb0888933
lemonkazi <lemonpstu09@gmail.com>
2022-03-08 15:07:14 +0600

    Fix: error solved

--- app/Traits/CreatedUpdatedBy.php
+++ app/Traits/CreatedUpdatedBy.php
@@ -1,26 +1,29 @@
 <?php
 
 namespace App\Traits;
+use Illuminate\Support\Facades\Auth;
 
 trait CreatedUpdatedBy
 {
     public static function bootCreatedUpdatedBy()
     {
-        // updating created_by and updated_by when model is created
-        static::creating(function ($model) {
-            if (!$model->isDirty('created_by') && auth()->user()) {
-                $model->created_by = auth()->user()->id;
-            }
-            if (!$model->isDirty('modified_by')) {
-                $model->modified_by = auth()->user()->id;
-            }
-        });
+        if(Auth::check()) {
+            // updating created_by and updated_by when model is created
+            static::creating(function ($model) {
+                if (!$model->isDirty('created_by')) {
+                    $model->created_by = Auth::user()->id;
+                }
+                if (!$model->isDirty('modified_by')) {
+                    $model->modified_by = auth()->user()->id;
+                }
+            });
 
-        // updating updated_by when model is updated
-        static::updating(function ($model) {
-            if (!$model->isDirty('modified_by') && auth()->user()) {
-                $model->modified_by = auth()->user()->id;
-            }
-        });
+            // updating updated_by when model is updated
+            static::updating(function ($model) {
+                if (!$model->isDirty('modified_by')) {
+                    $model->modified_by = Auth::user()->id;
+                }
+            });
+        }
     }
 }
