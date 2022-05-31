43308585407a29138e627ed34e95641ee0483487
Tarikul Islam <islm90@gmail.com>
2022-06-01 00:29:33 +0600

    Update permission-attr.php

--- config/permission-attr.php
+++ config/permission-attr.php
@@ -1,68 +1,79 @@
 <?php
 
 return [
-        // 'user-list',
-        // 'user-create',
-        // 'user-edit',
-        // 'user-delete',
+        'all' => [
+                // 'user-list',
+                // 'user-create',
+                // 'user-edit',
+                // 'user-delete',
 
-        // Role Controller
-        'role-list',
-        'role-create',
-        'role-edit',
-        'role-delete',
+                // Role Controller
+                'role-list',
+                'role-create',
+                'role-edit',
+                'role-delete',
 
-        // permission Controller
-        // 'permission-list',
-        // 'permission-create',
-        // 'permission-edit',
-        // 'permission-delete',
+                // permission Controller
+                // 'permission-list',
+                // 'permission-create',
+                // 'permission-edit',
+                // 'permission-delete',
 
-        // LanguageTranslations Controller
-        'translation-list',
-        'translation-create',
-        'translation-edit',
-        'translation-delete',
-        
-        // Language controller
-        'language-list',
-        'language-create-udpate',
+                // LanguageTranslations Controller
+                'translation-list',
+                'translation-create',
+                'translation-edit',
+                'translation-delete',
+                
+                // Language controller
+                'language-list',
+                'language-create-udpate',
 
-        // TermCondController controller
-        'terms-condition-list',
-        'terms-condition-add-udpate',
+                // TermCondController controller
+                'terms-condition-list',
+                'terms-condition-add-udpate',
 
-        // TermCondController controller
-        'email-template-list',
-        'email-template-add-udpate',
+                // TermCondController controller
+                'email-template-list',
+                'email-template-add-udpate',
 
-        // Schoool Permission SchoolController
-        'schools-list',
-        'schools-udpate',
-        'schools-user-udpate',
-        'schools-delete',
-        // Event parameters EventCategoryController
-        'parameters-list',
-        'parameters-create-udpate',
-        'parameters-delete',
+                // Schoool Permission SchoolController
+                'schools-list',
+                'schools-udpate',
+                'schools-user-udpate',
+                'schools-delete',
+                // Event parameters EventCategoryController
+                'parameters-list',
+                'parameters-create-udpate',
+                'parameters-delete',
 
-        // Teacher permission TeachersController
-        'teachers-list',
-        'teachers-view',
-        'teachers-create',
-        'teachers-update',
-        'teachers-users-update',
-        'teachers-delete',
+                // Teacher permission TeachersController
+                'teachers-list',
+                'teachers-view',
+                'teachers-create',
+                'teachers-update',
+                'teachers-users-update',
+                'teachers-delete',
 
-        // Student permission StudentsController
-        'students-list',
-        'students-view',
-        'students-create',
-        'students-update',
-        // 'students-users-update',
-        // 'students-delete',
-
-
-        ];
+                // Student permission StudentsController
+                'students-list',
+                'students-view',
+                'students-create',
+                'students-update',
+                // 'students-users-update',
+                // 'students-delete',
+        ],
+        'school_admin' => [
+          
+        ],
+        'teachers_admin' => [
+        ],
+        'teachers_all' => [
+        ],
+        'teachers_medium' => [
+        ],
+        'teachers_minimum' => [
+        ],
+];
 
 
