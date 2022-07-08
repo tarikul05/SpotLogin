d397e0bc2fc93836b4643a26515d3a870a71b1ba
Tarikul Islam <islm90@gmail.com>
2022-07-08 18:10:29 +0600

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -260,6 +260,7 @@ class UserController extends Controller
      */
     public function verify_user_added($token)
     {
+        
         try{
             $to = Carbon::now()->format("Y-m-d");
             $verifyToken = VerifyToken::where([
@@ -280,7 +281,7 @@ class UserController extends Controller
                     if ($verifyToken->person_type =='App\Models\Student') {
                         $user_data->email = $user_data->student_email;
                     }else{
-                        $user_data->email = $user_data->student_email;
+                        $user_data->email = $user_data->email;
                     }
                     return view('pages.verify.add')->with(compact('school','countries','genders','user_data','verifyToken'));
                 }else{
