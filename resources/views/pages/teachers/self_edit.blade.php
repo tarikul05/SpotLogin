8e777d6c2f1d70e148234b08daf93390795bc199
lemonkazi <cbb.mamun@cybridge.jp>
2022-07-12 18:16:58 +0600

    Fix: :bug: self update condition removed

--- resources/views/pages/teachers/self_edit.blade.php
+++ resources/views/pages/teachers/self_edit.blade.php
@@ -280,9 +280,9 @@
 							</div>
 						</div>
 					</fieldset>
-					@can('teachers-update')
-						<button type="submit" id="save_btn" name="save_btn" class="btn btn-success teacher_save"><i class="fa fa-save"></i>{{ __('Save') }}</button>
-					@endcan
+					
+					<button type="submit" id="save_btn" name="save_btn" class="btn btn-success teacher_save"><i class="fa fa-save"></i>{{ __('Save') }}</button>
+					
 				</form>
 			</div>
 			<div class="tab-pane fade" id="tab_2" role="tabpanel" aria-labelledby="tab_2">
@@ -347,9 +347,9 @@
 						</tbody>
 						</table>
 
-					@can('teachers-update')
+					
 						<button type="submit" id="save_btn" name="save_btn" class="btn btn-success teacher_save"><em class="glyphicon glyphicon-floppy-save"></em> {{ __('Save')}}</button>
-					@endcan
+					
 				</form>
 			</div>
 			<div class="tab-pane fade" id="tab_3" role="tabpanel" aria-labelledby="tab_3">
@@ -376,7 +376,7 @@
 													style="margin-right:10px;">
 										<?php endif; ?>
 
-											@can('teachers-update')
+											
 											<div style="display:flex;flex-direction: column;">
 												<div style="margin:5px;">
 													<span class="btn btn-theme-success">
@@ -397,7 +397,7 @@
 													</div>
 												<?php //endif; ?>
 											</div>
-											@endcan
+											
 										</div>
 									</fieldset>
 								</div>
