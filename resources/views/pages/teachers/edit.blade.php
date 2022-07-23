ddf54ccdba3c4c6aaeff1682d575b35339f794c8
Tarikul Islam <islm90@gmail.com>
2022-07-23 14:58:59 +0600

    remove enbale user account

--- resources/views/pages/teachers/edit.blade.php
+++ resources/views/pages/teachers/edit.blade.php
@@ -173,14 +173,14 @@
 										</div>
 									</div>
 								</div>
-								<div class="form-group row" id="shas_user_account_div">
+								<!-- <div class="form-group row" id="shas_user_account_div">
 									<div id="shas_user_account_div111" class="row">
 										<label class="col-lg-3 col-sm-3 text-left" for="shas_user_account" id="has_user_ac_label_id">{{__('Enable teacher account') }} :</label>
 										<div class="col-sm-7">
 											<input id="shas_user_account"  name="has_user_account" type="checkbox" value="1" {{!empty($relationalData->has_user_account) ? (old('has_user_account', $relationalData->has_user_account) == 1 ? 'checked' : '') : (old('has_user_account') == 1 ? 'checked' : '')}}>
 										</div>
 									</div>
-								</div>
+								</div> -->
 								<div class="form-group row" id="authorisation_div">
 										<label class="col-lg-3 col-sm-3 text-left"><span id="autorisation_caption">{{__('Authorization') }} :</span> </label>
 									<div class="col-sm-7">
