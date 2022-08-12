cce6335e6ddb8a4ab2bc60b0809836100b7ed7dd
Tarikul Islam <islm90@gmail.com>
2022-08-12 19:57:20 +0600

    provience sidder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
         $this->call(LessonPricesTableSeeder::class);
         $this->call(CurrencyTableSeeder::class);
         $this->call(EmailTemplateTableSeeder::class);
+        $this->call(ProvincesTableSeeder::class);
     }
 }
