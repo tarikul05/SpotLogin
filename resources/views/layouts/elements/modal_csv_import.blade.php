34757278048700a2b71570e23184b5f2825992d3
Tarikul Islam <islm90@gmail.com>
2022-08-27 02:48:37 +0600

    rename button and text

--- resources/views/layouts/elements/modal_csv_import.blade.php
+++ resources/views/layouts/elements/modal_csv_import.blade.php
@@ -6,7 +6,7 @@
       <div class="modal-header d-block text-center border-0">
         <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> -->
 
-        <h3 class="modal-title light-blue-txt gilroy-bold" id="importModalLabel">{{ __('Import CSV') }}</h3>
+        <h3 class="modal-title light-blue-txt gilroy-bold" id="importModalLabel">{{ __('Import Excel') }}</h3>
 
       </div>
       <div class="modal-body" style="max-width: 375px; margin: 0 auto;padding-top: 0;">
@@ -15,7 +15,7 @@
           <div class="form-group">
             <input type="file" name="csvFile" id="csvFile" accept=".csv"/>
           </div>
-          <button type="submit" class="btn btn-lg btn-primary btn-block">{{ __('Import CSV') }}</button>
+          <button type="submit" class="btn btn-lg btn-primary btn-block">{{ __('Import Excel') }}</button>
         </form>
       </div>
     </div>
