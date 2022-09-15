559ba7c80c98d5aabbf197312533ce5afeeedec9
Tarikul Islam <islm90@gmail.com>
2022-09-16 03:16:30 +0600

    update time grid

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -574,17 +574,17 @@ class AgendaController extends Controller
                 }
                 
                 if ($user->isTeacherAdmin()) {
-                    $e['tooltip']=' Students: '.$student_name.' <br/> '.$e['event_type_name'].' <br /> Duration: '.$fetch->duration_minutes;
+                    $e['tooltip']=' <br/>  Students: '.$student_name.' <br/> '.$e['event_type_name'].' <br /> Duration: '.$fetch->duration_minutes;
                 } else {
-                    $e['tooltip']=' Students: '.$student_name.' <br/> '.$e['event_type_name'].' <br /> Teacher: '.$e['teacher_name'].' <br /> Duration: '.$fetch->duration_minutes;
+                    $e['tooltip']=' <br/>  Students: '.$student_name.' <br/> '.$e['event_type_name'].' <br /> Teacher: '.$e['teacher_name'].' <br /> Duration: '.$fetch->duration_minutes;
                 }
 
                 if ($fetch->duration_minutes > 60) {
                     $e['title'] = '';
                     if ($user->isTeacherAdmin()) {
-                        $e['title_extend']= 'Students: '.$student_name.' <br/> '.$e['event_type_name'].' <br /> Duration: '.$fetch->duration_minutes;
+                        $e['title_extend']= ' <br/> Students: '.$student_name.' <br/> '.$e['event_type_name'].' <br /> Duration: '.$fetch->duration_minutes;
                     } else {
-                        $e['title_extend']= ' Students: '.$student_name.' <br/> '.$e['event_type_name'].' <br /> Teacher: '.$e['teacher_name'].' <br /> Duration: '.$fetch->duration_minutes;
+                        $e['title_extend']= ' <br/>  Students: '.$student_name.' <br/> '.$e['event_type_name'].' <br /> Teacher: '.$e['teacher_name'].' <br /> Duration: '.$fetch->duration_minutes;
                     }
                 }elseif($fetch->duration_minutes > 44){
                     $e['title']= $e['event_type_name'].' '.$student_name;
