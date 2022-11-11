2f18239874fb8cd3f22c49038b66f3516d930cb7
shahjalal7311 <mdjalal2012@gmail.com>
2022-11-12 00:22:22 +0600

    remove expired date from subscription list page

--- resources/views/pages/subscribers/list.blade.php
+++ resources/views/pages/subscribers/list.blade.php
@@ -13,25 +13,44 @@
                         <th>Name</th>
                         <th>Email</th>
                         <th>Plan Type</th>
-                        <th>Expired Date</th>
-                        <th>Next Payment</th>
                         <th>Price</th>
+                        <th>Next Payment</th>
                         <th>Action</th>
                     </tr>
                 </thead>
                 <thead>
                     <?php foreach($subscribers as $subscriber){ ?>
                     <tr>
-                        <th><?= $subscriber['user_name'] ?></th>
-                        <th><?= $subscriber['email'] ?></th>
-                        <th><span class="plan_type"><?= $subscriber['plan_name'] ?></span></th>
-                        <th><?= date('M j, Y', $subscriber['current_period_end']); ?></th>
-                        <th><?= date('M j, Y', $subscriber['billing_cycle_anchor']); ?></th>
-                        <th>
-                            <span class="price"><?= '$'.($subscriber['amount_decimal'])/100 ?></span>
-                            <span class="interval"><?= '/'.$subscriber['interval'] ?></span>
-                        </th>
-                        <th>
+                        <td><?= $subscriber['user_name'] ?></td>
+                        <td><?= $subscriber['email'] ?></td>
+                        <td>
+                            <?php if($subscriber['plan_name']){ ?>
+                                <span class="plan_type"><?= $subscriber['plan_name'] ?></span>
+                            <?php } else { ?>
+                                <span class="plan_type_free">Basic</span>
+                            <?php } ?>
+                        </td>
+                        <td>
+                            <?php 
+                                if(!empty($subscriber['amount_decimal'])){ ?>
+                                    <span class="price">
+                                        <?= '$'.($subscriber['amount_decimal'])/100;?>
+                                    </span>
+                                    <span class="interval"><?= '/'.$subscriber['interval'] ?></span>
+                            <?php }else{
+                                echo 'Free';
+                            } ?>
+                        </td>
+                        <td>
+                            <?php 
+                                if(!empty($subscriber['billing_cycle_anchor'])){
+                                    echo date('M j, Y', $subscriber['billing_cycle_anchor']);
+                                }else{
+                                    echo '--/--/----';
+                                }
+                            ?>
+                        </td>
+                        <td>
                             <a class="action_link" target="_blank" href="<?= $subscriber['invoice_url'] ?>">
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M7 17.0129L11.413 16.9979L21.045 7.4579C21.423 7.0799 21.631 6.5779 21.631 6.0439C21.631 5.5099 21.423 5.0079 21.045 4.6299L19.459 3.0439C18.703 2.2879 17.384 2.2919 16.634 3.0409L7 12.5829V17.0129ZM18.045 4.4579L19.634 6.0409L18.037 7.6229L16.451 6.0379L18.045 4.4579ZM9 13.4169L15.03 7.4439L16.616 9.0299L10.587 15.0009L9 15.0059V13.4169Z" fill="#657E8E"/>
@@ -39,7 +58,7 @@
                                 </svg>
                                 <span class="action_icon">Invoice</span>
                             </a>
-                        </th>
+                        </td>
                     </tr>
                     <?php } ?>    
                 </thead>
