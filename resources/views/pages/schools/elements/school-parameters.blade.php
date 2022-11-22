f9c0739743f2ab86fd66e415cda8292850d8d14b
Tarikul Islam <islm90@gmail.com>
2022-11-22 22:01:50 +0600

    udpates

--- resources/views/pages/schools/elements/school-parameters.blade.php
+++ resources/views/pages/schools/elements/school-parameters.blade.php
@@ -569,6 +569,7 @@ $(document).ready(function(){
     $(document).on('click', "input[name$=\'[s_thr_pay_type]\'][value='0']", function(event) {
         var dd = $(this).closest('.pack_invoice_area').find("input[name$=\'[s_std_pay_type]\'][value='2']").prop('checked')
         if (dd) {
+            alert("If the student is packaged the teacher can not be paid hourly")
             event.preventDefault();
         }
     });
