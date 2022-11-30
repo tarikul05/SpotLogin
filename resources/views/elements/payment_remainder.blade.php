6d9113b3958b5dbec15da51472e2099f2d47f6c3
shahjalal7311 <mdjalal2012@gmail.com>
2022-11-30 23:16:58 +0600

    add message reminder after subscription expired

--- resources/views/elements/payment_remainder.blade.php
+++ resources/views/elements/payment_remainder.blade.php
@@ -1,10 +1,16 @@
-@if( !$is_subscribed && (!empty($user->trial_ends_at) && ($today_date <= $ends_at)))
+@if( !$is_subscribed )
     <div class="subscription_info">
         <div class="container-fluid area-container">
             <div class="text-success">
+                <?php if( !empty($user->trial_ends_at) && ($today_date <= $ends_at) ){ ?>
                 <p class="text">
                     <span class="day_count"><?php echo $day_diff ?></span> days remaining in your trial period. <a href="{{ route('subscription.upgradePlan') }}"> Upgrade now! </a>
                 </p>
+                <?php }else{ ?>
+                    <p class="text">
+                        your trial period ended at <?= date('M j, Y, g:i a', strtotime($trial_ends_date)) ?>. <a href="{{ route('subscription.upgradePlan') }}"> Upgrade now! </a>
+                    </p>
+                <?php } ?>
             </div>
         </div>
     </div>
