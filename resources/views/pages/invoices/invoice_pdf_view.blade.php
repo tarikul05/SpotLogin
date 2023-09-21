9d104f4dcaeb9a12fd12307efc3f42e1e43dadf7
Steeve9569 <j.steeve@free.fr>
2023-09-21 16:14:34 +0200

    fix doc

--- resources/views/pages/invoices/invoice_pdf_view.blade.php
+++ resources/views/pages/invoices/invoice_pdf_view.blade.php
@@ -247,7 +247,7 @@
             <div class="right_part">
                 <div class="invoice_date">
                     <div><span class="txt">{{ __('date_of_invocie') }} </span><span class="date">{{ Carbon\Carbon::parse($invoice_data->date_invoice)->format('d.m.Y');}}</span></div>
-                    <div class="padding_top"><span class="txt">{{ __('due_date_of_invocie') }}</span><span class="date">{{ $invoice_data->date_due ? {{ Carbon\Carbon::parse($invoice_data->date_due)->format('d.m.Y')}} : Carbon\Carbon::parse($invoice_data->date_invoice)->addDays(7)->format('d.m.Y')}}</span></div>
+                    <div class="padding_top"><span class="txt">{{ __('due_date_of_invocie') }}</span><span class="date">{{ $invoice_data->date_due ?  Carbon\Carbon::parse($invoice_data->date_due)->format('d.m.Y') : Carbon\Carbon::parse($invoice_data->date_invoice)->addDays(7)->format('d.m.Y')}}</span></div>
                 </div>
             </div>
         </div>
