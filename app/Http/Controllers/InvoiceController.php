dbf6276080da15d0c883cd6078b45c958ee2d111
Steeve9569 <j.steeve@free.fr>
2024-01-27 15:38:34 +0100

    fix tasks

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -103,6 +103,7 @@ class InvoiceController extends Controller
         //$invoices = Invoice::orderByRaw('DATE_FORMAT(date_invoice, "%Y-%m-%d %H:%i:%s") DESC');
         $results =  $invoices = $invoices->where('school_id', $schoolId)->get();
 
+
     return view('pages.invoices.list', compact('timeZone','school', 'invoices', 'schoolId', 'invoice_type_all', 'payment_status_all'));
     }
 
