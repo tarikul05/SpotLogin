fcf9ed1a5a4f0e3cc2e1fa1b2bcd6d75401c0d02
Steeve9569 <j.steeve@free.fr>
2024-04-02 16:48:42 +0200

    fix tasks

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2056,10 +2056,11 @@ class InvoiceController extends Controller
             $pdf->set_option('isHtml5ParserEnabled', true);
             $pdf->set_option('isRemoteEnabled', true);
             $pdf->set_option('DOMPDF_ENABLE_CSS_FLOAT', true);
+            $pdf->render();
             // print and save data
             if ($type == 'stream') {
                 // save invoice name if invoice_filename is empty
-                $file_upload = Storage::put('pdf/'. $invoice_name, $pdf->output());
+                $file_upload = Storage::put('pdf/'. $invoice_name, $pdf);
                 if($file_upload){
                     $invoice_pdf_path = URL::to("").'/uploads/pdf/'.$invoice_name;
                     $invoice_data->invoice_filename = $invoice_pdf_path;
