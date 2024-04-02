c415df4d7d8764ad7b59aae77f7030b98c81cfe9
Steeve9569 <j.steeve@free.fr>
2024-04-02 15:05:18 +0200

    fix tasks

--- resources/views/pages/invoices/invoice_pdf_view.blade.php
+++ resources/views/pages/invoices/invoice_pdf_view.blade.php
@@ -244,7 +244,7 @@
         <div class="logo_area">
             <div class="left_part">
                 <?php if ($invoice_data->logo !== null): ?>
-                <img class="img_logo" src="{{ public_path($invoice_data->logo_url) }}" alt="" style="height: 50px;">
+                <img class="img_logo" src="{{ public_path($invoice_data->logo) }}" alt="" style="height: 50px;">
                 <?php endif; ?>
             </div>
             <div class="right_part">
