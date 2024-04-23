54cc02d1af3428b397708dd04439559d14f7d30c
Steeve9569 <j.steeve@free.fr>
2024-04-23 19:09:11 +0200

    fix tasks

--- resources/views/pages/calendar/edit_lesson.blade.php
+++ resources/views/pages/calendar/edit_lesson.blade.php
@@ -1027,9 +1027,9 @@ $("#student, #teacher_select, #duration").on('change', function(event) {
                             	$(".priceByStudent").text(response.eventPrice['price_buy']);
 							}
                         } else {
-							$("#sprice_amount_sell").val(response.lessonPriceTeacher['price_sell'])
+							$("#sprice_amount_sell").val(response.eventPrice['price_buy'])
                     	    var newDuration = $("#duration").val();
-                            $(".priceByStudent").text(response.lessonPriceTeacher['price_sell']);
+                            $(".priceByStudent").text(response.eventPrice['price_buy']);
 
 						}
 
