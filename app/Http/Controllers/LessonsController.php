0fa3c07a873678633bd63e8abb1eee7bb4ab2ee8
Steeve9569 <j.steeve@free.fr>
2024-06-29 13:57:05 +0200

    fix

--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -1403,14 +1403,14 @@ class LessonsController extends Controller
 
             if(!empty($studentCount)){
                 if($lessonPriceTeacher && $lessonPriceTeacher['lesson_price_student'] === "price_fix") {
-                    $buyPriceCal = ($lessonPriceTeacher['price_buy']*($duration/60));
+                    $buyPriceCal = ($lessonPriceTeacher['price_sell']*($duration/60));
                     $sellPriceCal = ($lessonPriceTeacher['price_buy']*($duration/60)/$lessonData['no_of_students']);
                 } else {
-                    $buyPriceCal = ($eventPrice['price_buy']*($duration/60));
+                    $buyPriceCal = ($eventPrice['price_sell']*($duration/60));
                     $sellPriceCal = ($eventPrice['price_buy']*($duration/60));
                 }
             }else{
-                $buyPriceCal = ($eventPrice['price_buy']*($duration/60));
+                $buyPriceCal = ($eventPrice['price_sell']*($duration/60));
                 $sellPriceCal = ($eventPrice['price_buy']*($duration/60));
             }
 
