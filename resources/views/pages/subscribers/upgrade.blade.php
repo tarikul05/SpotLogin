20d5fe72f030812e7398959af7f4328e52245023
Steeve9569 <j.steeve@free.fr>
2024-11-25 13:42:30 +0100

    fix

--- resources/views/pages/subscribers/upgrade.blade.php
+++ resources/views/pages/subscribers/upgrade.blade.php
@@ -5,7 +5,9 @@
   background-color: #f6f9fc;
 }
 
-
+.applePayBtn {
+  margin-top:25px!important;
+}
 
 .example.example4 form {
   max-width: 496px !important;
@@ -514,7 +516,8 @@
                                     <input type="text" style="font-size:15px; color:#333;" class="form-control" id="card_holder_name" name="card_holder_name" placeholder="{{ __('Enter Cardholder full name') }}" value="{{ Auth::user()->firstname .' ' . Auth::user()->lastname }}" required>
                                 </div>
                                 
-                                <div id="payment-request-button"></div>
+                                <div id="payment-request-button" class="applePayBtn"></div>
+                                <div id="payment-request-divider"><br>-- or --<br></div>
 
                                 <div class="example4"></div>
                                 <div id="example4-paymentRequest">
