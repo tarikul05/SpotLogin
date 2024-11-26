06f8d55667b507c7590de3f5d56aee42dbbe6746
Steeve9569 <j.steeve@free.fr>
2024-11-26 12:09:34 +0100

    fix

--- resources/views/pages/invoices/invoice_modification.blade.php
+++ resources/views/pages/invoices/invoice_modification.blade.php
@@ -2135,7 +2135,7 @@ function extractExtraCharges($inputString) {
       });
 
       var amountApplePay = "{{$invoice->total_amount}}";
-      var countryCodeApplePay = "{{$user->selectedSchoolCountryCode()}}";
+      var countryCodeApplePay = "{{$AppUI->selectedSchoolCountryCode()}}";
       var InvoiceIdLabel = "{{$invoice->id}}";
 
       var paymentRequest = stripe.paymentRequest({
