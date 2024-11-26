937d692739b430fd22426ef7490549fbab191897
Steeve9569 <j.steeve@free.fr>
2024-11-26 17:30:58 +0100

    fix

--- app/Http/Controllers/StripeTransactionController.php
+++ app/Http/Controllers/StripeTransactionController.php
@@ -7,6 +7,7 @@ use Stripe\Stripe;
 use Stripe\PaymentIntent;
 use Stripe\PaymentMethod;
 use Stripe\Invoice;
+use Stripe\Charge;
 use Illuminate\Support\Facades\Auth;
 
 class StripeTransactionController extends Controller
@@ -15,40 +16,32 @@ class StripeTransactionController extends Controller
     {
         $user = Auth::user();
 
-        // Configure la clé Stripe
         Stripe::setApiKey(env('STRIPE_SECRET'));
 
-        // Récupère les Payment Intents liés au client
         $paymentIntents = PaymentIntent::all([
             'customer' => $user->stripe_id,
             'limit' => 100,
-        ])->data; // Récupère le tableau brut des Payment Intents
+        ])->data; 
 
-        // Convertit en collection pour mapper
         $transactions = collect($paymentIntents)->map(function ($transaction) {
-            // Vérifie si une méthode de paiement est associée
             if (isset($transaction->payment_method)) {
-                // Récupère les détails de la méthode de paiement
                 $paymentMethod = PaymentMethod::retrieve($transaction->payment_method);
-
                 if (isset($transaction->invoice)) {
-                $invoice = Invoice::retrieve($transaction->invoice);
-                // Ajoute les détails à l'Invoice
-                $transaction->invoice_pdf = $invoice->invoice_pdf;
+                    $invoice = Invoice::retrieve($transaction->invoice);
+                    $transaction->invoice_pdf = $invoice->invoice_pdf;
+                } else {
+                    $charge = Invoice::retrieve($transaction->latest_charge);
+                    $transaction->invoice_pdf = $charge->receipt_url;
                 }
-
-                // Ajoute les détails au Payment Intent
                 $transaction->card_last4 = $paymentMethod->card->last4 ?? null;
                 $transaction->card_brand = $paymentMethod->card->brand ?? null;
             } else {
                 $transaction->card_last4 = null;
                 $transaction->card_brand = null;
             }
-
             return $transaction;
         });
 
-        // Retourne la vue avec les données
         return view('pages.transactions.index', [
             'transactions' => $transactions,
             'user' => $user,
